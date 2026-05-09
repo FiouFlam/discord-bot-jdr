9bd5b195ade6a36d0088dd1fafd7081b757a7e3b
FiouFlam <111578668+FiouFlam@users.noreply.github.com>
2026-05-09 15:50:41 +0200

    Delete src directory

--- src/utils/session.js
+++
@@ -1,5 +0,0 @@
-// Stockage en mémoire des sessions actives
-// sessions : Map<guildId, { userIds: string[], message: Message | null }>
-const sessions = new Map();
-
-module.exports = { sessions };
